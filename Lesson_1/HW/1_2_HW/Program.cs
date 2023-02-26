a3a1330441aadabb2b9a22a2f409661329e0b0ae
Serg <newday7171@mail.ru>
2023-02-26 11:04:18 +0700

    Task 1_2_HW

---
+++ Lesson_1/HW/1_2_HW/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход 
+//три числа и выдаёт максимальное из этих чисел.
+
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int c = int.Parse(Console.ReadLine()!);
+
+int max_number = a;
+
+if (b > max_number)
+    max_number = b;
+if (c > max_number)
+    max_number = c;
+Console.WriteLine("Максимальное число " + max_number);
