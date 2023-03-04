1f49cf5e927b0218411cc11179b66db8ef81ea5d
Serg <newday7171@mail.ru>
2023-03-04 16:36:56 +0700

    Task 2_2_HW

---
+++ Lesson_2/HW/2_2_HW/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 3: Напишите программу, которая выводит третью 
+//цифру заданного числа или сообщает, что третьей цифры нет.
+
+int ThirdRank(int num)
+{
+    while (num > 999)
+    {
+        num = num / 10;
+    }
+    return num % 10;
+}
+
+bool Validate (int num)
+{
+    if (num < 100)
+    {
+        Console.WriteLine("Третьей цифры нет");
+        return false;
+    }
+    return true;
+}    
+
+int num = int.Parse(Console.ReadLine()!);
+if (Validate(num))
+{
+    Console.WriteLine(ThirdRank(num));
+}
+
