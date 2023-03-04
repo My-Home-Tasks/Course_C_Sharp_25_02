423d4c197777871516c132446271e104ea739df3
Serg <newday7171@mail.ru>
2023-03-04 15:26:30 +0700

    Task 2_1_HW

---
+++ Lesson_2/HW/2_1_HW/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 2. Напишите программу, которая выводит случайное 
+// трехзначное число и удаляет вторую цифру этого числа.
+
+int TakeNum(int num)
+{
+    Console.WriteLine(num);
+    int Second = ((num / 100) * 10) + (num % 10) ;
+    return Second;
+}
+
+int itog = TakeNum(new Random().Next(100, 1000));
+Console.WriteLine(itog);
+
